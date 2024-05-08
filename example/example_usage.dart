75780d03843dd0aa11c5f75f520d16288a5c1f7c
Amine Dakhli <amine.dakhli@medtech.tn>
2024-05-08 17:24:10 +0100

    adding post request handling example

--- example/example_usage.dart
+++ example/example_usage.dart
@@ -10,9 +10,25 @@ void main() async {
   });
 
   server.post('/', (Request req, Response res) {
-    res
-      ..send(req.body["key"])
-      ..close();
+    var name = req.body['name'];
+    var age = req.body['age'];
+
+    // Perform some operations with the data
+    if (name != null && age != null) {
+      // Send a response indicating success and echo the received data
+      res.status(200); // HTTP 200 OK
+      res.json({
+        'message': 'Data received successfully',
+        'yourData': {
+          'name': name,
+          'age': age
+        }
+      });
+    } else {
+      // Send an error response if required data is missing
+      res.status(400); // HTTP 400 Bad Request
+      res.send('Missing name or age in the request body');
+    }
   });
 
   server.put('/', (Request req, Response res) {
